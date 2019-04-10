0f105711d78d2facd7043ea92d0b8519a17e0d99
Darnell Martin <darnellrmartin91@gmail.com>
2019-04-10 01:24:47 -0400

    Added support for SQLAlchemy and specifying app location

--- app/build.py
+++ app/build.py
@@ -1,6 +1,7 @@
 import yaml
 import sqlite3
 import os
+import sys
 from jinja2 import Template, Environment, FileSystemLoader
 
 config_data = None
@@ -16,11 +17,7 @@ env = Environment(loader=file_loader, trim_blocks=True, lstrip_blocks=True)
 
 #Python Templates
 app_template = env.get_template('app.py.j2')
-db_template = env.get_template('db.py.j2')
-model_template = env.get_template('model.py.j2')
-
-#SQL Templates
-create_template = env.get_template('create.sql.j2')
+models_template = env.get_template('models.py.j2')
 
 #HTML Templates
 index_template = env.get_template('index.html.j2')
@@ -28,43 +25,32 @@ form_template = env.get_template('form.html.j2')
 
 # Insert Variables
 app_output = app_template.render(Application=config_data["Application"] , Model=config_data["Model"] ,Database=config_data["Database"])
-db_output = db_template.render(Application=config_data["Application"] , Model=config_data["Model"] ,Database=config_data["Database"])
-model_output = model_template.render(Application=config_data["Application"] , Model=config_data["Model"] ,Database=config_data["Database"])
-create_output = create_template.render(Application=config_data["Application"] , Model=config_data["Model"] ,Database=config_data["Database"])
+models_output = models_template.render(Application=config_data["Application"] , Model=config_data["Model"] ,Database=config_data["Database"])
 index_output = index_template.render(Application=config_data["Application"] , Model=config_data["Model"] ,Database=config_data["Database"])
 form_output = form_template.render(Application=config_data["Application"] , Model=config_data["Model"] ,Database=config_data["Database"])
 
 # Build Folder Structure
-if not os.path.exists("build"):
-    os.mkdir("build")
-if not os.path.exists("build/app"):
-    os.mkdir("build/app")
-if not os.path.exists("build/app/templates"):
-    os.mkdir("build/app/templates")
+if not os.path.exists(config_data["Application"]["Location"]):
+    os.mkdir(config_data["Application"]["Location"])
+if not os.path.exists(config_data["Application"]["Location"]+"/app"):
+    os.mkdir(config_data["Application"]["Location"]+"/app")
+if not os.path.exists(config_data["Application"]["Location"]+"/app/templates"):
+    os.mkdir(config_data["Application"]["Location"]+"/app/templates")
 
 # Write New Files
-with open("build/app/app.py","w+") as new_file:
+with open(config_data["Application"]["Location"]+"/app/app.py","w+") as new_file:
     new_file.write(app_output)
 
-with open("build/app/db.py","w+") as new_file:
-    new_file.write(db_output)
+with open(config_data["Application"]["Location"]+"/app/models.py","w+") as new_file:
+    new_file.write(models_output)
 
-with open("build/app/model.py","w+") as new_file:
-    new_file.write(model_output)
-
-with open("build/app/templates/index.html","w+") as new_file:
+with open(config_data["Application"]["Location"]+"/app/templates/index.html","w+") as new_file:
     new_file.write(index_output)
 
-with open("build/app/templates/form.html","w+") as new_file:
+with open(config_data["Application"]["Location"]+"/app/templates/form.html","w+") as new_file:
     new_file.write(form_output)
 
 #Build DB
-try:
-    exists = os.path.isfile("build/app/app.db")
-    if exists:
-        os.remove("build/app/app.db")
-    conn = sqlite3.connect("build/app/app.db")
-    c = conn.cursor()
-    c.execute(create_output)
-except Error as e:
-    print(e)
+sys.path.append(config_data["Application"]["Location"]+"/app")
+from app import db
+db.create_all()
